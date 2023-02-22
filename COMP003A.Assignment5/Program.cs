f5f004421d8d65637b7a66d2368abb1e3175a0f4
RubenVillagrana <RubenVillagrana35@gmail.com>
2023-02-21 21:50:13 -0800

    I'm confused

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -6,55 +6,58 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
 
-namespace ClearConsole
+namespace PrintSeparator
 
 {
     class Program
     {
-        static void PrintSeparator();
-        
-        {
-         
+        static void PrintSeparator()
 
-        static void Main(string[] args)
         {
 
 
-
-            Console.WriteLine("**************************************************************");
-            Console.WriteLine("Triangle Section");
-            Console.WriteLine("*************************************************************");
-            Console.WriteLine("Enter a Single Character:");
-            Console.WriteLine("Enter a Positive Whole Number");
-
-
-            static void PrintSeparater(int N)
+            static void Main(string[] args)
             {
 
 
-                int i, j;
 
-                // Loop to print the upper half
-                // diamond pattern
-                for (i = 0; i < N; i++)
-                {
-                    for (j = 0; j < i + 1; j++)
-                        Console.Write("*");
-                    Console.Write("\n");
-                }
+                Console.WriteLine("**************************************************************");
+                Console.WriteLine("Triangle Section");
+                Console.WriteLine("*************************************************************");
+                Console.WriteLine("Enter a Single Character:");
+                Console.WriteLine("Enter a Positive Whole Number");
 
-                // Loop to print the lower half
-                // diamond pattern
-                for (i = 1; i < N; i++)
+
+                static void PrintSeparater(int N)
                 {
-                    for (j = i; j < N; j++)
-                        Console.Write("*");
-                    Console.Write("\n");
+
+                    char[] arr = new char[N];
+                    int i, j;
+
+                    // Loop to print the upper half
+                    // diamond pattern
+                    for (i = 0; i < N; i++)
+                    {
+                        for (j = 0; j < i + 1; j++)
+                            Console.Write("*");
+                        Console.Write("\n");
+                    }
+
+                    // Loop to print the lower half
+                    // diamond pattern
+                    for (i = 1; i < N; i++)
+                    {
+                        for (j = i; j < N; j++)
+                            Console.Write("*");
+                        Console.Write("\n");
+                    }
                 }
             }
         }
     }
 }
+
+
     
 
 
