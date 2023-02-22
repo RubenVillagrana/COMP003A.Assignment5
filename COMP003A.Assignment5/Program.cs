a8314a8c5a35717a2d86684939f38c91ee58372d
RubenVillagrana <RubenVillagrana35@gmail.com>
2023-02-22 04:39:41 -0800

    jesus....lol

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -11,52 +11,51 @@ namespace PrintSeparator
 {
     class Program
     {
-        static void PrintSeparator()
+        //PrintSeparator with no output set up outside of the main
+        static void PrintSeparator(int N)
 
         {
 
 
             static void Main(string[] args)
             {
-                
+
                 Console.WriteLine("**************************************************************");
                 Console.WriteLine("Triangle Section");
                 Console.WriteLine("*************************************************************");
                 Console.WriteLine("Enter a Single Character:");
                 Console.WriteLine("Enter a Positive Whole Number");
 
+                
+                        int i, j;
+                        int N = 50;
+                        // Loop to print the upper half
+                        // diamond pattern
+                        for (i = 0; i < N; i++)
+                        {
+                            for (j = 0; j < i + 1; j++)
+                                Console.Write("*");
+                            Console.Write("\n");
+                        }
 
-                static void PrintSeparater(int N)
-                {
-
-                    char[] arr = new char[N];
-                    int i, j;
-
-                    // Loop to print the upper half
-                    // diamond pattern
-                    for (i = 0; i < N; i++)
-                    {
-                        for (j = 0; j < i + 1; j++)
-                            Console.Write("*");
-                        Console.Write("\n");
-                    }
-
-                    // Loop to print the lower half
-                    // diamond pattern
-                    for (i = 1; i < N; i++)
-                    {
-                        for (j = i; j < N; j++)
-                            Console.Write("*");
-                        Console.Write("\n");
+                        // Loop to print the lower half
+                        // diamond pattern
+                        for (i = 1; i < N; i++)
+                        {
+                            for (j = i; j < N; j++)
+                                Console.Write("*");
+                            Console.Write("\n");
+                        }
                     }
                 }
             }
         }
-    }
-}
-
-
     
+
+
+
+
+ 
 
 
     
